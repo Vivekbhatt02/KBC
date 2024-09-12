ce2d67ab4e5ee3a8c40a9d9ad1dad0637508adc0
Vivekbhatt02 <159415297+Vivekbhatt02@users.noreply.github.com>
2024-09-12 19:54:15 +0530

    Add files via upload
    Welcome to the KBC (Kaun Banega Crorepati) simulation project! This repository contains a Python-based implementation of the popular quiz show, KBC. The program emulates the format and style of the show, allowing users to test their knowledge through a series of multiple-choice questions and shows the prize money they won.

---
+++ kbc.py
@@ -0,0 +1,95 @@
+x = "Wecome to KBC"
+print(x.center(50))
+y = "Here is your first question for 10000 rupees"
+print(y.center(50))
+print("1.Who wrote India's national anthem?")
+print("your options are:\n"
+      "a:Rabindranath Tagore\n"
+      "b:Lal Bahadur Shastri\n"
+      "c:Chetan Bhagat\n"
+      "d:RK Narayan")
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = input("Choose the correct option:")
+if e == a:
+  print("You win the 10000 rupees")
+else:
+  print("You lose,Better luck next time")
+  exit()
+
+z = "Here is your second question for 50000 rupees"
+print(z.center(50))
+print("2.Who is the first prime minister of india?")
+print("your options are:\n"
+      "a:Manmohan Singh\n"
+      "b:Lal Bahadur Shastri\n"
+      "c:Narendra Modi\n"
+      "d:Jawaharlal Nehru")
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = input("Choose the correct option:")
+if e == d:
+  print("You win the 60000 rupees")
+else:
+  print("You lose,Better luck next time")
+  exit()
+z = "Here is your third question for 100000 rupees"
+print(z.center(50))
+print("3.Who is the first president of india?")
+print("your options are:\n"
+      "a:Dr.Sarvepalli Radhakrishan\n"
+      "b:Dr.Rajendra Prasad\n"
+      "c:Draupadi Murmu\n"
+      "d:Dr.APJ Abdul Kalam")
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = input("Choose the correct option")
+if e == b:
+  print("You win the 160000 rupees")
+else:
+  print("You lose,Better luck next time")
+  exit()
+z = "Here is your fourth question for 200000 rupees"
+print(z.center(50))
+print(
+  "4.Who holds the 6 times world record holder in paraolympic javelin throw?")
+print("your options are:\n"
+      "a:Manish Narwal\n"
+      "b:Mona Aggrawal\n"
+      "c:Sumit Antil\n"
+      "d:Navdeep Singh")
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = input("Choose the correct option:")
+if e == c:
+  print("You win the 360000 rupees")
+else:
+  print("You lose,Better luck next time")
+  exit()
+z = "Here is your fifth question for 500000 rupees"
+print(z.center(50))
+print("5.Which team won the ipl 2024?")
+print("your options are:\n"
+      "a:KKR\n"
+      "b:RR\n"
+      "c:DC\n"
+      "d:RCB")
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = input("Choose the correct option:")
+w = "You win the 860000 rupees"
+if e == a:
+  print(w)
+else:
+  print("You lose,Better luck next time")
+  exit()
